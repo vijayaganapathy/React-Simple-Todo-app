14234bc29655deae9d3a3d721446133cd0dcdbc5
vijayaganapathy <42263071+vijayaganapathy@users.noreply.github.com>
2019-09-26 00:00:58 -0700

    Create Nav.js

---
+++ Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class navfunction extends React.Component{
+    render(){
+        return(<div>
+            <h1 class="text-secondary" style={{textAlign:"center"}}>My To-Do App</h1>
+        </div>
+        );
+        }
+}
+export default navfunction;
