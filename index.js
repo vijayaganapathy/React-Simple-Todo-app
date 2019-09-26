a6a4de4a1d7882ae495de51b14943a48c8d45459
vijayaganapathy <42263071+vijayaganapathy@users.noreply.github.com>
2019-09-25 23:51:52 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css";
+import App1 from "./App";
+
+ReactDOM.render(<App1/>, document.getElementById('root'));
